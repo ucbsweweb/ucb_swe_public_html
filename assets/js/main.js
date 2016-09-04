58d9f8f978ef16ce46d397829079c03957008e34
anranli <lianran@gmail.com>
2016-09-03 21:27:10 -0700

    redesign the website

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+/**
+ * UC Berkeley SWE
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('sweweb', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/national", {templateUrl: "partials/national.html", controller: "Membership"})
+    .when("/committee", {templateUrl: "partials/committee.html", controller: "Membership"})
+    .when("/GMslides", {templateUrl: "partials/GMslides.html", controller: "ResourceCtrl"})
+    .when("/newsletters", {templateUrl: "partials/newsletters.html", controller: "ResourceCtrl"})
+    .when("/alumnisignup", {templateUrl: "partials/alumnisignup.html", controller: "ResourceCtrl"})
+    .when("/outreach", {templateUrl: "partials/outreach.html", controller: "ResourceCtrl"})
+    .when("/sciencesat", {templateUrl: "partials/sciencesat.html", controller: "ResourceCtrl"})
+    .when("/sciencescholar", {templateUrl: "partials/sciencescholar.html", controller: "ResourceCtrl"})
+    .when("/corporatepackage", {templateUrl: "partials/corporatepackage.html", controller: "BlogCtrl"})
+    .when("/calendar", {templateUrl: "partials/calendar.html", controller: "BlogCtrl"})
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/postjob", {redirectTo: ("https://docs.google.com/forms/d/e/1FAIpQLSeuHZNz-8IC_lN8T4N710FZQeU25OfmQfntWsyJFMpZXJbHCQ/viewform?embedded=true")})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+
+    .when("/sponsors", {templateUrl: "partials/sponsors.html", controller: "SponsorCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
+
+app.controller('MembershipCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
