3dac375df8731c2c1324a2cb2b2815323a320a03
Shirley <apricot@Shirleys-MBP-3.fios-router.home>
2018-05-24 00:01:23 -0700

    cleaned up repo

--- bin/sendEmail-v1.55/BACKUP/gm/optimizePDF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Requires Ghostscript
-#For further compression, try adding '-dPDFSETTINGS=/screen' (optimize for viewing on a screen)
-
-in=$1
-fname=`basename $in .pdf`
-out="${fname}_opt.pdf"
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4  -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$out $in
--- old/officers/genContactPages/commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./parseOfficerInfo.py contact.csv fun.csv tmp/
-cp contacts.shtml ../../contacts.shtml
-cp tmp/* ../individual/
